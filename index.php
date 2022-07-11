20ca3478ba338ec77e1bb9c9470d47a38e541ca5
fonzhousefonzie <harth.fonz@gmail.com>
2022-07-11 17:17:25 +0200

    completed

---
+++ index.php
@@ -0,0 +1,389 @@
+<?php
+$nav = [
+    'Introduzione',
+    'Norme sulla privacy',
+    'Termini di servizio',
+    'Tecnologie',
+    'Domande frequenti'
+];
+
+$page = [
+    [
+        'title' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+        'content' => [
+            "La recente <a href='#'>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+            "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
+            "Per presentare una richiesta di rimozione, compila questo <a href='#'>modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
+            "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
+            "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
+        ]
+    ],
+    [
+        'title' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        'content' => [
+            "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
+            "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
+            "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href='#'>Centro Google per la sicurezza online</a>.",
+            "<a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
+        ],
+    ],
+    [
+        'title' => "Perché il mio account è associato a un paese?",
+        'content' => [
+            "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+            "<ol>
+                <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                    <ol type='a'>
+                        <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                        <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+                    </ol>
+                </li>
+                <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+            </ol>",
+            "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
+            "<h3>Stabilire il paese associato al tuo account</h3>",
+            "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.",
+            "I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.",
+            "Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href='#'>Contattaci <svg xmlns='http://www.w3.org/2000/svg' width='17' height='17' viewBox='0 0 24 24' fill='#3367d6'><path d='M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z'/></svg></a> se ritieni che il paese associato al tuo account sia sbagliato."
+        ],
+    ],
+    [
+        'title' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        'content' => [
+            "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href='#'>fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>."
+        ],
+    ],
+    [
+        'title' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+        'content' => [
+            "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli <a href='#'>URL referrer</a>. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
+        ]
+    ]
+];
+
+$footerNav = [
+    "Google",
+    "Tutto su Google",
+    "Privacy",
+    "Termini"
+];
+
+$languageOptions = [
+    '<option value="af">Afrikaans</option>',
+    '<option value="id">Bahasa Indonesia</option>',
+    '<option value="ms">Bahasa Melayu</option>',
+    '<option value="ca">Català</option>',
+    '<option value="cs">Čeština</option>',
+    '<option value="da">Dansk</option>',
+    '<option value="de">Deutsch</option>',
+    '<option value="et">Eesti</option>',
+    '<option value="en">English</option>',
+    '<option value="en-GB">English (United Kingdom)</option>',
+    '<option value="es">Español</option>',
+    '<option value="es-419">Español (Latinoamérica)</option>',
+    '<option value="eu">Euskara</option>',
+    '<option value="fil">Filipino</option>',
+    '<option value="fr">Français</option>',
+    '<option value="fr-CA">Français (Canada)</option>',
+    '<option value="gl">Galego</option>',
+    '<option value="hr">Hrvatski</option>',
+    '<option value="zu">Isizulu</option>',
+    '<option value="is">Íslenska</option>',
+    '<option selected="" value="it">Italiano</option>',
+    '<option value="sw">Kiswahili</option>',
+    '<option value="lv">Latviešu</option>',
+    '<option value="lt">Lietuvių</option>',
+    '<option value="hu">Magyar</option>',
+    '<option value="nl">Nederlands</option>',
+    '<option value="no">Norsk</option>',
+    '<option value="pl">Polski</option>',
+    '<option value="pt-BR">Português (Brasil)</option>',
+    '<option value="pt-PT">Português (Portugal)</option>',
+    '<option value="ro">Română</option>',
+    '<option value="sk">Slovenčina</option>',
+    '<option value="sl">Slovenščina</option>',
+    '<option value="sr-Latn">Srpski</option>',
+    '<option value="fi">Suomi</option>',
+    '<option value="sv">Svenska</option>',
+    '<option value="vi">Tiếng Việt</option>',
+    '<option value="tr">Türkçe</option>',
+    '<option value="el">Ελληνικά</option>',
+    '<option value="bg">Български</option>',
+    '<option value="ru">Русский</option>',
+    '<option value="sr">Српски</option>',
+    '<option value="uk">Українська</option>',
+    '<option value="iw">‫עברית‬</option>',
+    '<option value="ur">‫اردو‬</option>',
+    '<option value="ar">‫العربية‬</option>',
+    '<option value="fa">‫فارسی‬</option>',
+    '<option value="am">አማርኛ</option>',
+    '<option value="mr">मराठी</option>',
+    '<option value="hi">हिन्दी</option>',
+    '<option value="bn">বাংলা</option>',
+    '<option value="gu">ગુજરાતી</option>',
+    '<option value="ta">தமிழ்</option>',
+    '<option value="te">తెలుగు</option>',
+    '<option value="kn">ಕನ್ನಡ</option>',
+    '<option value="ml">മലയാളം</option>',
+    '<option value="th">ไทย</option>',
+    '<option value="ko">한국어</option>',
+    '<option value="zh-HK">中文 (香港)</option>',
+    '<option value="zh-CN">中文（简体中文）</option>',
+    '<option value="zh-TW">中文（繁體中文）</option>',
+    '<option value="ja">日本語</option>'
+]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domande frequenti - Privacy e termini - Google</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="d-flex justify-content-between align-items-center">
+        <div class="heading d-flex gap-1 align-items-center">
+            <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="Google Logo">
+            <h1>Privacy e termini</h1>
+        </div>
+        <div class="menu d-flex gap-4 align-items-center">
+            <svg class="gb_Ue" focusable="false" viewBox="0 0 24 24">
+                <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+            </svg>
+            <img class="user" src="https://lh3.googleusercontent.com/ogw/ADea4I5zvGDfE-eVQnzw-p4oyDceUIY5tByYnEd34O80=s64-c-mo" alt="">
+        </div>
+    </header>
+    <nav>
+        <ul class="d-flex list-unstyled">
+            <?php foreach ($nav as $item) {
+                if ($item === 'Domande frequenti') {
+                    echo '<li class="active">' . $item . '</li>';
+                } else {
+                    echo '<li>' . $item . '</li>';
+                }
+            } ?>
+        </ul>
+    </nav>
+    <main>
+        <div class="container">
+            <?php
+            foreach ($page as $article) {
+                echo "<h2>" . $article['title'] . "</h2>";
+                foreach ($article['content'] as $paragraph) {
+                    if (stripos($paragraph, '<ol>') === 0 || stripos($paragraph, '<h3>') === 0) {
+                        echo $paragraph;
+                    } else {
+                        echo "<p>" . $paragraph . "</p>";
+                    }
+                }
+            } ?>
+        </div>
+    </main>
+    <footer>
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="footer-menu d-flex">
+                <?php
+                foreach ($footerNav as $index => $link) {
+                    if ($index === 0) {
+                        echo '<a href="#">' . $link . '</a>';
+                    } else {
+                        echo '<span>&middot;</span><a href="#">' . $link . '</a>';
+                    }
+                } ?>
+            </div>
+            <div class="language d-flex">
+                <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAABEAAAARCAMAAAAMs7fIAAAABGdBTUEAALGPC/xhBQAAAFpQTFRFAAAAc8b%2BeNj%2BeLL7lt3/YbL4cs/9rNn8p9b7%2Bfn5hdD/a8D/7vD/vOL9itf%2B////3PD%2Byuj9R6348Pb7KZz2UL78MKL3Oqv4Zsz/ndX8qN/%2BI5T0S7X4W8b9mmuivQAAAAF0Uk5TAEDm2GYAAACXSURBVBjTXYqLEsIwCARR%2B27zpCFNxP//TSGOTu3OhDs2wGaPM9bD8hKGMDtnDq130GkxDK/BY/tVc6CtXa21Q72ClFKoqUrINBLAzHP9grKpccxFYmqNIcY4j%2BXDiLJBztmUXCRkGgkgIsZbO1kwEjVDKwYmDriSGt6FPHrnp6x1Bd/vzxP9Bj8mFfBH2K8Gtng1AA95bxQhE7o%2B68UQAAAAAElFTkSuQmCC" alt="">
+                <select class="iCthae" id="i3" jsname="O1htCb">
+                    <?php 
+                    foreach($languageOptions as $option){
+                        echo $option;
+                    } ?>
+                </select>
+            </div>
+        </div>
+    </footer>
+</body>
+
+<style>
+    @font-face {
+        font-family: 'Product Sans';
+        src: url('font/Product Sans Regular.ttf');
+        font-weight: normal;
+    }
+
+    @font-face {
+        font-family: 'Product Sans';
+        src: url('font/Product Sans Thin.ttf');
+        font-weight: 300;
+    }
+
+    @font-face {
+        font-family: 'Product Sans';
+        src: url('font/Product Sans Medium.ttf');
+        font-weight: 600;
+    }
+
+    @font-face {
+        font-family: 'Product Sans';
+        src: url('font/Product Sans Bold.ttf');
+        font-weight: bold;
+    }
+
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+    :root {
+        --bs-font-sans-serif: 'Product Sans', 'Roboto', sans-serif;
+        --text-color: #5f6368;
+        --lighter-text-color: #bbc2c9;
+        --darker-text-color: #3c4043;
+        --active: #3367d6;
+        --footer: #f5f5f5;
+        --footer-text: rgba(61,61,61,.8);
+    }
+
+    header {
+        padding: 1.2rem 1.3rem 0.5rem;
+        cursor: not-allowed;
+    }
+
+    h1 {
+        font-size: 22px;
+        color: var(--text-color);
+        font-weight: 500;
+        padding-bottom: 0.2rem;
+        margin: 0;
+    }
+
+    h2 {
+        font-size: 24px;
+        color: var(--darker-text-color);
+        font-weight: 600;
+        padding-top: 64px;
+        line-height: 2rem;
+        letter-spacing: 1px;
+    }
+
+    h3 {
+        font-size: 18px;
+        font-weight: 600;
+        padding: 52px 0 0;
+        line-height: 26px;
+        color: var(--darker-text-color);
+    }
+
+    main {
+        padding: 2rem 0;
+    }
+
+    main .container p {
+        margin-bottom: 10px;
+        padding-top: 20px;
+        font-size: 14px;
+        line-height: 24px;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    main .container ol {
+        padding: 0;
+        margin: 0 0 16px 16px;
+    }
+
+    main .container li {
+        padding: 12px 0 0 16px;
+        font-size: 14px;
+        line-height: 24px;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    a {
+        text-decoration: none;
+        color: var(--active);
+    }
+
+    img,
+    svg {
+        max-height: 24px;
+    }
+
+    svg {
+        min-width: 24px;
+    }
+
+    path {
+        fill: var(--text-color);
+    }
+
+    img.user {
+        max-height: 32px;
+        border-radius: 50%;
+    }
+
+    nav{
+        cursor: not-allowed;
+    }
+
+    nav ul {
+        padding: 0;
+        margin: 0;
+        border-bottom: 1px solid var(--lighter-text-color);
+        gap: 3rem;
+    }
+
+    nav ul li {
+        padding: 0.5rem 0;
+        color: var(--text-color);
+        font-size: 1rem;
+    }
+
+    nav ul li:first-child {
+        padding-left: 1.5rem;
+    }
+
+    nav .active {
+        color: var(--active);
+        border-bottom: 3px solid var(--active);
+    }
+
+    .container {
+        max-width: 978px;
+        padding: 0;
+    }
+
+    footer{
+        background: var(--footer);
+        padding: 10px;
+    }
+
+    footer a{
+        color: var(--footer-text);
+        font-size: 11px;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 300;
+        padding: 0;
+    }
+
+    footer span{
+        color: var(--footer-text);
+        font-family: 'Roboto', sans-serif;
+        font-weight: 300;
+        font-size: 11px;
+        padding: 0 5px;
+    }
+
+    footer select{
+        font-size: 13.3333px;
+    }
+
+    footer img{
+        height: 24px;
+        width: auto;
+        padding: 2px 5px;
+    }
+</style>
+
+</html>
